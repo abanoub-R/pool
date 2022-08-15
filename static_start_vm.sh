ca04c27fcf6176dad76b254dca4ee8aaf1f02227
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-08-15 08:32:10 -0500

    Update static_start_vm.sh

--- static_start_vm.sh
+++ static_start_vm.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# THIS SCRIPT WAS TAKEN FROM WIKI.GENTOO.ORG WITH BOOT OPTIONS ADDED 
+# IN ITS CURRENT STATE, THIS IS MEANT FOR A GENTOO ISO, TO USE IT ON
+# YOUR SYSTEM, CHANGE THE OPTIONS BELOW.
+
 exec qemu-system-x86_64 -enable-kvm \
         -cpu host \
         -drive file=images/vm-gentoo.img,if=virtio \
