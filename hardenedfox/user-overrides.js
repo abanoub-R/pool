a8f1f6c7031d961ca3bb406b9fbc87abd5d41b58
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-08-22 17:28:37 -0500

    added quality of life features
    now supports the new tab page and remembering logins

--- hardenedfox/user-overrides.js
+++ hardenedfox/user-overrides.js
@@ -10,6 +10,17 @@ user_pref("privacy.resistFingerprinting", false);
 // remember sign-ons
 user_pref("signon.rememberSignons", true);
 user_pref("network.cookie.lifetimePolicy", 0);
+user_pref("signon.autofillForms", true);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("browser.sessionstore.privacy_level", 0);
+
+// restore home page
+user_pref("browser.startup.page", 1);
+user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.newtabpage.enabled", true);
 
 // enable searching with url bar
 user_pref("keyword.enabled", true);
+
+// cosmetic
+user_pref("widget.non-native-theme.gtk.scrollbar.round-thumb	false");
