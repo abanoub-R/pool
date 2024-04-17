45080c4e1dc263279a81645fbd977d64345a30ee
abanoub <90291414+abanoub-R@users.noreply.github.com>
2024-04-17 14:15:04 -0700

    Add files via upload

---
+++ scripts/py/tesseract.py
@@ -0,0 +1,21 @@
+# NOTE: below code expects png,
+#       if not in png format,
+#       destination format
+#       will be:
+#
+#       basename.ext.txt
+
+from PIL import Image
+
+import pytesseract
+import os
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+for x in os.listdir('sources'):
+    destination = open(f"results/{x}.txt", "a")
+    for y in os.listdir(f"sources/{x}"):
+        print(f"on file {y}")
+        destination.write(pytesseract.image_to_string(Image.open(f"sources/{x}/{y}")))
+    destination.close()
+    print(f"wrote all contents to results/{x}.txt")
